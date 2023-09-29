d6b62893e3840e9aa80e4e690cd06fa5f1182b3b
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-09-29 04:30:45 +0000

    Pending changes exported from your codespace

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -1,23 +1,43 @@
-// domain.com/auth/signin
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-// add this in meta tag for responsiveness
-// <meta name="viewport" content="initial-scale=1, width=device-width" />
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './signInStyles.css';
+import { Poppins } from 'next/font/google';
+import { Input } from '@mui/material';
 
 
-
-function SignIn(){
-    return (<div className='Container'>
-        <LeftComponent/>
-        <RightComponent/>
-    </div>);
+function signIn(){
+    return <div className='container'>
+        <LeftCont/>
+    </div> 
 }
 
-const RightComponent=()=>{
-    return <h1>This is Right typography</h1>
+const LeftCont=()=>{
+    return <section className='leftCont'>
+        <div className='form'>
+            <Typography variant='h3' align='left' fontWeight={800} >Login</Typography>
+            <Input size='large' variant='caption' sx={{ marginTop: 5}} fullWidth label="userName" placeholder="Username" />
+            <Input size='small' variant='caption' type='password' sx={{ marginTop: 1, marginBottom: 2}} fullWidth label="password" placeholder="Password" />
+            <Link  variant='caption' href="" target="_blank" rel="noopener noreferrer" color="#000000">Forgot Password?</Link>
+            <br/>
+            <Button sx={{borderRadius:2, marginTop:5, backgroundColor:'#4046E6', color:'white', '&:hover':{backgroundColor:'#262ce9'}}} textTransform="none" fullWidth variant='Contained'>Login</Button>
+            
+            <Typography variant='caption' align='left'>Don't have an account</Typography>
+            <Button sx={{borderRadius:2, marginTop:5, backgroundColor:'#333437', color:'white', '&:hover':{backgroundColor:'#222'}}} textTransform="none" align='right' variant='Contained'>Sign Up</Button>
+       
+        </div>
+    </section>
 }
 
-const LeftComponent=()=>{
-    return <h1>This is Left typography</h1>
-}
-
-export default SignIn;
+export default signIn;
